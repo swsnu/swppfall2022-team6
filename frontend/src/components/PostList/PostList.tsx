ea44c03623dfe3b921a7c7df5a01e76bf01f0f26
Cheeun Lim <86710785+ice1039@users.noreply.github.com>
2022-10-28 20:19:13 +0900

    Post and Post List with fixed errors (#18)
    * Set postProps for postList and Post
    
    * Post Test Done
    
    * Post List test covered 100
    
    * fixed errors

--- frontend/src/components/PostList/PostList.tsx
+++ frontend/src/components/PostList/PostList.tsx
@@ -1,39 +1,60 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Post, { PostType } from "../Post/Post";
+import Post from "../Post/Post";
+
+type PostType = {
+    id: number,
+    user_id: number,
+    content: string,
+    image: string,    // image url
+    latitude: number,
+    longitude: number,
+    time: string, // date string
+    reply_to: number    // id of the chained post
+};
 
 function PostList() {
-    const onClickAddPostButton = () => {};
+    const onClickAddPostButton = () => {}; // should be implemented with postModal
     const navigate = useNavigate();
+    
 
     const [allPosts, setAllPosts] = useState<PostType[]>([
         {
             id: 2,
             user_id: 2,
-            content: "�б��� ���� ��׿�Ф�\n�ѿ� ì��ô°� ���� �� ���ƿ�!",
+            content: "학교는 많이 춥네요ㅠㅠ\n겉옷 챙기시는게 좋을 것 같아요!",
             latitude: 37.44877599087201,
             longitude: 126.95264777802309,
             time: new Date().toLocaleDateString(),
             reply_to: 1,
-            chain_open: false,
+            image: "",
         },
         {
             id: 1,
             user_id: 1,
             content:
-                "���� ������ ���� �ѵ� �ٶ��� ���� �Ҿ��\n�ѿ��� �� ì�� ���Դµ� �б��� �䳪��? �ڿ��� �ʿ�...",
+                "지금 설입은 맑긴 한데 바람이 많이 불어요\n겉옷을 안 챙겨 나왔는데 학교도 춥나요? 자연대 쪽에...",
             latitude: 37.44877599087201,
             longitude: 126.95264777802309,
             time: new Date().toLocaleDateString(),
+            image: "",
             reply_to: 0,
         },
     ]);
 
-    // map ���ؼ� lat, long ó�� + chain_open, username �߰��� postProps
+    // get user from backend with user_id
+    const users: {user_name: string, user_id: number}[] = [
+        {"user_name": 'WeatherFairy', "user_id": 1},
+        {"user_name": 'Toothfairy', "user_id": 2}
+    ];
+
+    const post_location = "Bongcheon-dong, Gwanak-gu"; //should be implemented with Map API, 
 
     const clickPostHandler = (post: PostType) => {
         navigate("/areafeed/" + post.id);
     };
+
+    const isChainOpen = false;  //default is false when rendered, should be toggled in redux state
 
     return (
         <div id="PostList">
@@ -42,15 +63,17 @@ function PostList() {
                     {allPosts.map((post) => {
                         return (
                             <Post
+                                key={post.id}
                                 id={post.id}
-                                user_id={post.user_id}
+                                user_name={users.find((user) => user.user_id === post.user_id)!.user_name}
                                 content={post.content}
-                                latitude={post.latitude}
-                                longitude={post.longitude}
+                                location={post_location} //should come from map API
                                 time={post.time}
                                 reply_to={post.reply_to}
-                                chain_open={post.chain_open}
+                                image={""}
+                                chain_open={isChainOpen}  //default is false when rendered
                                 clickPost={() => clickPostHandler(post)}
+                                // toggleChain={} for chain open/close w redux
                             />
                         );
                     })}
