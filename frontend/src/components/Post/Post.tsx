8c102793277ecc706ea8bfb24d7d9c5567f154e8
ice1039 <jl6880@daum.net>
2022-10-28 15:53:21 +0900

    Set postProps for postList and Post

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,56 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Chart } from "react-chartjs-2";
+import { Navigate, useNavigate } from "react-router";
 
-export interface PostType {
-    id: number;
-    user_id: number;
-    content: string;
-    image?: string; // image url
-    latitude: number;
-    longitude: number;
-    time: string; // date string
-    reply_to: number; // id of the chained post
-    chain_open?: boolean; // chain_open should not be implemented in back though?
+
+export interface postProps {
+    id: number,
+    user_name: string,
+    content: string,
+    image: string,    // image url, "" if none
+    location: string,
+    time: string,       // date & time string
+    reply_to: number,    // id of the chained post, 0 if none
+    chain_open: boolean, // default is false, sent from PostList
     clickPost?: React.MouseEventHandler<HTMLDivElement>; // Click the post
 }
-
-// get user from backend with user_id
-const users: { user_name: string; user_id: number }[] = [
-    { user_name: "WeatherFairy", user_id: 1 },
-    { user_name: "Toothfairy", user_id: 2 },
-];
-
 // get location from user lang, long
-const post_location = "Bongcheon-dong, Gwanak-gu";
 
-const Post = (post: PostType) => {
-    const user = users.find((user) => user.user_id === post.user_id);
-    // const toggleChainHandler = () => {
-    //     post.chain_open = !post.chain_open;
-    // };
-
+const Post = (post: postProps) => {
+    
+    const toggleChainHandler = () => {
+        post.chain_open = !post.chain_open;
+    };
+    
     return (
+    <div id="post-and-chain-container">
         <div id="post-container" onClick={post.clickPost}>
             <div id="user-container">
                 <div id="user-main-badge"></div>
-                <div id="user-name">{user?.user_name}</div>
-                <div id="location">{post_location}</div>
-                <div id="timestamp">{post.time}</div>
+                <div id="user-name">
+                    {post.user_name}
+                </div>
+                <div id="location">
+                    {post.location}
+                </div>
+                <div id="timestamp">
+                    {post.time}
+                </div>
             </div>
             <div id="post-content-container">
-                <div id="post-text">{post.content}</div>
-                <div id="post-photo"></div>
-            </div>
-            <div id="chain-container">
-                <div id="chained-posts"></div>
-                <div id="chain-toggle">
-                    {post.reply_to === 0 ? null : post.chain_open === true ? (
-                        <button id="chain-toggle-button">Show All</button>
-                    ) : (
-                        <button id="chain-toggle-button">Close All</button>
-                    )}
+                <div id="post-text">
+                    {post.content}
+                </div>
+                <div id="post-photo">
                 </div>
             </div>
         </div>
+        <div id="chain-container">
+            <div id="chained-posts">
+            </div>
+            <div id="chain-toggle">
+                {post.reply_to === 0
+                ? null
+                : (post.chain_open === false 
+                    ? <button id="chain-toggle-button">Show All</button>
+                    : <button id="chain-toggle-button">Close All</button>)
+                }
+            </div>
+        </div>
+    </div>
     );
 };
 
