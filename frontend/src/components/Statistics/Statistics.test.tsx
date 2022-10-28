e23895bb1f1193261449a6346862c54bea20b07f
lilys012 <lysianthus@naver.com>
2022-10-28 17:49:52 +0900

    travis.yml fix

--- frontend/src/components/Statistics/Statistics.test.tsx
+++ frontend/src/components/Statistics/Statistics.test.tsx
@@ -6,14 +6,25 @@ import React from "react";
 jest.mock("react-chartjs-2", () => ({
     Bar: () => <div>Bar Chart</div>,
 }));
+jest.mock("react-minimal-pie-chart", () => ({
+    PieChart: () => <div>Pie Chart</div>,
+}));
+const mockRegister = jest.fn();
+jest.mock("chart.js", () => ({
+    ...jest.requireActual("chart.js"),
+    register: () => mockRegister,
+}));
 
 describe("<Statistics />", () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
     it("should render without errors", () => {
-        render(<Statistics />);
+        const { container } = render(<Statistics />);
+        expect(container.querySelector("#statistics-container")).toBeTruthy();
         screen.getByText("Bar Chart");
+        screen.getByText("Pie Chart");
+        // expect(mockRegister).toHaveBeenCalled();
     });
     // it("should not show anything if no reports", () => {
     //     const mockedSetform = jest.fn();
