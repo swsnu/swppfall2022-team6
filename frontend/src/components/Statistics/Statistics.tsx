8f47755a84b86b08fbaa5439783e793776b3d09f
lilys012 <lysianthus@naver.com>
2022-10-30 22:27:59 +0900

    zero reports handled

--- frontend/src/components/Statistics/Statistics.tsx
+++ frontend/src/components/Statistics/Statistics.tsx
@@ -53,16 +53,7 @@ export const Baroptions = {
 const labels = ["Sunny", "Wind", "Happy", "Humidity"];
 
 function Statistics() {
-    const [allReports, setAllReports] = useState<ReportType[]>([
-        {
-            weather: "Sunny",
-            weather_degree: 2,
-            wind_degree: 1,
-            happy_degree: 2,
-            humidity_degree: 5,
-            time: "",
-        },
-    ]);
+    const [allReports, setAllReports] = useState<ReportType[]>([]);
     const [maxIndex, setMaxIndex] = useState<number>(0);
 
     useEffect(() => {
@@ -72,136 +63,145 @@ function Statistics() {
             })
             .then((response) => {
                 setAllReports(response.data);
-            })
-            .then(() => {
-                const lenArray: number[] = [0, 0, 0, 0];
-                lenArray[0] = allReports.filter(
-                    (report) => report.weather === "Sunny"
-                ).length;
-                lenArray[1] = allReports.filter(
-                    (report) => report.weather === "Wind"
-                ).length;
-                lenArray[2] = allReports.filter(
-                    (report) => report.weather === "Happy"
-                ).length;
-                lenArray[3] = allReports.filter(
-                    (report) => report.weather === "Humidity"
-                ).length;
-                setMaxIndex(lenArray.indexOf(Math.max(...lenArray)));
             });
     }, []);
+
+    useEffect(() => {
+        const lenArray: number[] = [0, 0, 0, 0];
+        allReports.forEach((report) => {
+            if (report.weather === "Sunny") lenArray[0]++;
+            else if (report.weather === "Cloudy") lenArray[1]++;
+            else if (report.weather === "Rain") lenArray[2]++;
+            else lenArray[3]++;
+        });
+        setMaxIndex(lenArray.indexOf(Math.max(...lenArray)));
+    }, [allReports]);
 
     return (
         <div
             id="statistics-container"
             style={{ display: "flex", justifyContent: "space-around" }}
         >
-            <div
-                id="piechart-container"
-                style={{ width: "150px", height: "200px" }}
-            >
-                <PieChart
-                    data={[
-                        {
-                            title: "Sunny",
-                            value: allReports.filter(
-                                (report) => report.weather === "Sunny"
-                            ).length,
-                            color: "#F6CB44",
-                        },
-                        {
-                            title: "Cloudy",
-                            value: allReports.filter(
-                                (report) => report.weather === "Wind"
-                            ).length,
-                            color: "#E3A454",
-                        },
-                        {
-                            title: "Rain",
-                            value: allReports.filter(
-                                (report) => report.weather === "Happy"
-                            ).length,
-                            color: "#76BEE3",
-                        },
-                        {
-                            title: "Snow",
-                            value: allReports.filter(
-                                (report) => report.weather === "Humidity"
-                            ).length,
-                            color: "#654321",
-                        },
-                    ]}
-                    lineWidth={18}
-                    background="#f3f3f3"
-                    lengthAngle={360}
-                    rounded
-                    animate
-                    label={({ dataEntry, dataIndex }) =>
-                        dataIndex === maxIndex
-                            ? dataEntry.title +
-                              " " +
-                              Math.ceil(dataEntry.percentage).toString() +
-                              "%"
-                            : ""
-                    }
-                    labelStyle={{ fontSize: "13px", fill: "#33333" }}
-                    labelPosition={0}
-                    viewBoxSize={[100, 100]}
-                />
-            </div>
-            <Bar
-                options={Baroptions}
-                data={{
-                    labels,
-                    datasets: [
-                        {
-                            label: "Statistics",
-                            data: [
-                                allReports
-                                    .filter(
-                                        (report) =>
-                                            report.weather === labels[maxIndex]
-                                    )
-                                    .map((report) => report.weather_degree)
-                                    .reduce(
-                                        (a: number, b: number) => a + b,
-                                        0
-                                    ) /
-                                    allReports
-                                        .filter(
-                                            (report) =>
-                                                report.weather ===
-                                                labels[maxIndex]
-                                        )
-                                        .map((report) => report.weather_degree)
-                                        .length,
-                                allReports.reduce(
-                                    (a: number, b: ReportType) =>
-                                        a + b.wind_degree,
-                                    0
-                                ) / allReports.length,
-                                allReports.reduce(
-                                    (a: number, b: ReportType) =>
-                                        a + b.happy_degree,
-                                    0
-                                ) / allReports.length,
-                                allReports.reduce(
-                                    (a: number, b: ReportType) =>
-                                        a + b.humidity_degree,
-                                    0
-                                ) / allReports.length,
+            {allReports.length ? (
+                <div>
+                    <div
+                        id="piechart-container"
+                        style={{ width: "150px", height: "200px" }}
+                    >
+                        <PieChart
+                            data={[
+                                {
+                                    title: "Sunny",
+                                    value: allReports.filter(
+                                        (report) => report.weather === "Sunny"
+                                    ).length,
+                                    color: "#F6CB44",
+                                },
+                                {
+                                    title: "Cloudy",
+                                    value: allReports.filter(
+                                        (report) => report.weather === "Cloudy"
+                                    ).length,
+                                    color: "#E3A454",
+                                },
+                                {
+                                    title: "Rain",
+                                    value: allReports.filter(
+                                        (report) => report.weather === "Rain"
+                                    ).length,
+                                    color: "#76BEE3",
+                                },
+                                {
+                                    title: "Snow",
+                                    value: allReports.filter(
+                                        (report) => report.weather === "Snow"
+                                    ).length,
+                                    color: "#654321",
+                                },
+                            ]}
+                            lineWidth={18}
+                            background="#f3f3f3"
+                            lengthAngle={360}
+                            rounded
+                            animate
+                            label={({ dataEntry, dataIndex }) =>
+                                dataIndex === maxIndex
+                                    ? dataEntry.title +
+                                      " " +
+                                      Math.ceil(
+                                          dataEntry.percentage
+                                      ).toString() +
+                                      "%"
+                                    : ""
+                            }
+                            labelStyle={{ fontSize: "13px", fill: "#33333" }}
+                            labelPosition={0}
+                            viewBoxSize={[100, 100]}
+                        />
+                    </div>
+                    <Bar
+                        options={Baroptions}
+                        data={{
+                            labels,
+                            datasets: [
+                                {
+                                    label: "Statistics",
+                                    data: [
+                                        allReports
+                                            .filter(
+                                                (report) =>
+                                                    report.weather ===
+                                                    labels[maxIndex]
+                                            )
+                                            .map(
+                                                (report) =>
+                                                    report.weather_degree
+                                            )
+                                            .reduce(
+                                                (a: number, b: number) => a + b,
+                                                0
+                                            ) /
+                                            allReports
+                                                .filter(
+                                                    (report) =>
+                                                        report.weather ===
+                                                        labels[maxIndex]
+                                                )
+                                                .map(
+                                                    (report) =>
+                                                        report.weather_degree
+                                                ).length,
+                                        allReports.reduce(
+                                            (a: number, b: ReportType) =>
+                                                a + b.wind_degree,
+                                            0
+                                        ) / allReports.length,
+                                        allReports.reduce(
+                                            (a: number, b: ReportType) =>
+                                                a + b.happy_degree,
+                                            0
+                                        ) / allReports.length,
+                                        allReports.reduce(
+                                            (a: number, b: ReportType) =>
+                                                a + b.humidity_degree,
+                                            0
+                                        ) / allReports.length,
+                                    ],
+                                    backgroundColor: [
+                                        "pink",
+                                        "lightblue",
+                                        "yellow",
+                                        "lightgreen",
+                                    ],
+                                },
                             ],
-                            backgroundColor: [
-                                "pink",
-                                "lightblue",
-                                "yellow",
-                                "lightgreen",
-                            ],
-                        },
-                    ],
-                }}
-                style={{ position: "relative", height: "200px" }}
-            />
+                        }}
+                        style={{ position: "relative", height: "200px" }}
+                    />
+                </div>
+            ) : (
+                <span>No Statistics!</span>
+            )}
         </div>
     );
 }
