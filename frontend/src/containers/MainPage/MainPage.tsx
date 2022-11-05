1f584d54f0e5bae88ec00b09d6bc1385eb6f2071
Seoyoon Moon <seoyoon5635@gmail.com>
2022-11-06 07:35:24 +0900

    Main css (#63)
    * Add css file for main
    
    * Add css file for main
    
    * Rebase main
    
    * Add css file for main
    
    * Update css to scss
    
    * Complete main style
    
    * Add click event on X
    
    * Add Close reult button
    
    * Fix AreaFeed bug

--- frontend/src/containers/MainPage/MainPage.tsx
+++ frontend/src/containers/MainPage/MainPage.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Map, { PositionType } from "./../../components/Map/Map";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
-import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBullhorn, faUser } from "@fortawesome/free-solid-svg-icons";
+
+import Map, { PositionType } from "./../../components/Map/Map";
 import AreaFeed from "../AreaFeed/AreaFeed";
 import ReportModal from "../../components/ReportModal/ReportModal";
 import MapSearch from "../../components/MapSearch/MapSearch";
+import "./MainPage.scss"
 
 const marks = [
     { value: 0, label: "0km" },
@@ -25,20 +29,41 @@ function MainPage() {
     const [openReport, setOpenReport] = useState<boolean>(false);
     const [markPosition, setMarkPosition] =
         useState<PositionType>(initMarkPosition);
+    const [currPosition, setCurrPosition] =
+        useState<PositionType>(initMarkPosition);
+    const [address, setAddress] = useState<string>("");
     const navigate = useNavigate();
 
-    useEffect(() => {
+    const geocoder = new kakao.maps.services.Geocoder();
+    
+    useEffect(()=>{
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 setMarkPosition({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
+                    lat: position.coords.latitude, 
+                    lng: position.coords.longitude
+                });
+                setCurrPosition({
+                    lat: position.coords.latitude, 
+                    lng: position.coords.longitude
                 });
             });
         } else {
             console.log("Geolocation is not supported by this browser.");
         }
     }, []);
+
+    useEffect(()=>{
+        geocoder.coord2RegionCode(
+            currPosition.lng, currPosition.lat, 
+          (result, status)=>{
+            console.log(result)
+            if(status == kakao.maps.services.Status.OK && !!result[0].address_name){
+              setAddress(result[0].address_name)
+            }
+          } 
+        ); 
+      }, [currPosition])
 
     const onClickMyPageIcon = () => {
         navigate("/mypage");
@@ -58,47 +83,49 @@ function MainPage() {
             <div id="upper-container">
                 NowSee
                 <button id="mypage-button" onClick={onClickMyPageIcon}>
-                    MyPage
+                    <FontAwesomeIcon icon={faUser} size="2x"/>
                 </button>
             </div>
-            <MapSearch
-                markPosition={markPosition}
-                setMarkPosition={setMarkPosition}
-            />
-            <Map initPosition={markPosition} radius={radius} />
-            <div id="lower-map-container">
-                <div className="radius-slider-container">
-                    <p>Change Radius</p>
-                    <Box sx={{ width: 300 }} id="radius-slider">
-                        <Slider
-                            key="radius-slider"
-                            aria-label="Custom marks"
-                            defaultValue={radius}
-                            step={2.5}
-                            min={0.01}
-                            getAriaValueText={(value: number): string => {
-                                return `${value}km`;
-                            }}
-                            valueLabelDisplay="auto"
-                            valueLabelFormat={(value: number): string => {
-                                return `${value / 25}km`;
-                            }}
-                            onChange={onChangeMapRadius}
-                            marks={marks}
-                        />
-                    </Box>
+            <div id="main-container">
+                <MapSearch markPosition={markPosition} setMarkPosition={setMarkPosition}/>
+                <div className="map-container">
+                    <Map initPosition={markPosition} radius={radius} />
                 </div>
-                <div className="findout-container">
-                    <button id="findout-button" onClick={onClickFindOutButton}>
-                        Find out
+                <div id="lower-map-container">
+                    <div className="radius-slider-container">
+                        <p>Change Radius</p>
+                        <Box sx={{ width: 300 }} id="radius-slider">
+                            <Slider
+                                key="radius-slider"
+                                aria-label="Custom marks"
+                                defaultValue={radius}
+                                step={2.5}
+                                min={0.01}
+                                getAriaValueText={(value: number): string => {
+                                    return `${value}km`;
+                                }}
+                                valueLabelDisplay="auto"
+                                valueLabelFormat={(value: number): string => {
+                                    return `${value / 25}km`;
+                                }}
+                                onChange={onChangeMapRadius}
+                                marks={marks}
+                            />
+                        </Box>
+                    </div>
+                    <div className="findout-container">
+                        <button id="findout-button" onClick={onClickFindOutButton}>
+                            {"Find out  >"}
+                        </button>
+                    </div>
+                </div>
+                <div id="bottom-container">
+                    <span>{`Current location: ${address}`}</span>
+                    <button id="report-button" onClick={onClickReportButton}>
+                        <span>Report!</span>
+                        {/* <FontAwesomeIcon icon={faBullhorn}/> */}
                     </button>
                 </div>
-            </div>
-            <div id="bottom-container">
-                <span>Current location: </span>
-                <button id="report-button" onClick={onClickReportButton}>
-                    Report
-                </button>
             </div>
             <ReportModal
                 openReport={openReport}
