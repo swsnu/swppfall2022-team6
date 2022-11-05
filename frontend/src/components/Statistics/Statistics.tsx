1373c60ab67fc4492bce01b43e4ba1885d60d838
Seoyoon <seoyoon5635@gmail.com>
2022-11-05 13:52:38 +0900

    Update to get once in statistics file

--- frontend/src/components/Statistics/Statistics.tsx
+++ frontend/src/components/Statistics/Statistics.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import * as d3 from 'd3';
 import { PieChart } from "react-minimal-pie-chart";
 import {
     Chart as ChartJS,
@@ -30,6 +31,11 @@ export interface ReportType {
     humidity_degree: number;
     time: string;
 }
+
+export interface dataType {
+    weather: string;
+    range: number;
+  }
 
 export const Baroptions = {
     indexAxis: "y" as const,
@@ -76,12 +82,114 @@ function Statistics() {
     const [maxIndex, setMaxIndex] = useState<number>(0);
     const [reportPerc, setReportPerc] = useState<number[]>([0, 0, 0, 0]);
 
+    const barRef = useRef<SVGElement>();
+
     const displaylabels = [
         ["☀️ ", "☁️ ", "☔ ", "❄️ "][maxIndex] + labels[maxIndex],
         "💨 Wind",
         "🤗 Happy",
         "💧 Humidity",
     ];
+
+    const data: dataType[] = []
+        for(let i=0; i<reportPerc.length; i++){
+          data.push({
+            weather: displaylabels[i],
+            range: reportPerc[i],
+          })
+        }
+
+        const [w, h] = [400, 140]; 
+        const barHeight = 15;
+        const svgElement = barRef.current as SVGElement;
+        // set the dimensions and margins of the graph
+        const margin = {top: 20, right: 20, bottom: 30, left: 90},
+        width = w - margin.left - margin.right,
+        height = h - margin.top - margin.bottom;
+
+        // append the svg object to the body of the page
+        const svg = d3.select(svgElement)
+        .call(g => g.select("g").remove())
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+        
+        const y = d3.scalePoint() 
+          .range([ 0, height ])
+          .domain(data.map(d=>d.weather));
+
+          // .padding(.1)
+
+        const x = d3.scaleLinear()
+          .domain([0, 5.5]) 
+          .range([ 0, width]) 
+
+        svg.selectAll(".bar-background")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr('rx', barHeight/2)
+          .attr('ry', barHeight/2)
+          //@ts-ignore
+          .attr("y", d=>{return y(d.weather)+(y.bandwidth()-barHeight)/2})
+          .attr("height", barHeight)
+          .attr("x", x(0)) 
+          .attr("width", x(5))
+          .attr("fill", "#EDF9FF");
+
+        const bar = svg.selectAll(".bar-data")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          // .join(
+          //   (enter)=>enter.append(".bar"),
+          //   (update)=>update.attr("class", "bar"),
+          //   (exit)=>exit.remove() 
+          // )
+          .attr('rx', barHeight/2)
+          .attr('ry', barHeight/2)
+          //@ts-ignore
+          .attr("y", d=>{return y(d.weather)+(y.bandwidth()-barHeight)/2}) 
+          .attr("x", x(0)) 
+          .attr("height", barHeight)
+            .attr("x",  d => { return x(0); })
+            .attr("width", 0)
+                .transition()
+                .duration(750)
+                .delay(function (d, i) {
+                    return i * 150;
+                })
+          //@ts-ignores
+          .attr("width", d=>x(d.range))
+          .attr("fill", "#3185E7") 
+          .attr("border", 0);
+        
+        // label은 다음에 찾아보는걸로..
+        svg.selectAll(".text")
+          .data(data)
+        .enter().append("text")
+          .attr("class", "label")
+          //@ts-ignore
+          .attr("y", function(d) { return y(d.weather) + (y.bandwidth()+10)/2; })
+          .attr("x", x(5.5))
+          .text(d=>{ return Math.round(d.range*20)+"%" })
+          .style("text-anchor", "middle")
+          .style("font-family", "NanumGothic") 
+          .style("font-family", "sans-serif")
+          .style("font-weight", "700")
+          .style("font-size", "15px")
+          .style("color", "rgba(0,0,0,50%)");
+          
+        svg.append("g")
+          .call(d3.axisLeft(y))
+          .style("stroke-width", 0)
+          .style("font-family", "NanumGothic") 
+          .style("font-family", "sans-serif")
+          .style("font-weight", "700")
+          .style("font-size", "15px")
+          .style("color", "rgba(0,0,0,75%)");
 
     useEffect(() => {
         axios
@@ -200,7 +308,10 @@ function Statistics() {
                             viewBoxSize={[100, 100]}
                         />
                     </div>
-                    <SmallStatistics />
+                    <div className="bar-container">
+                        {/* @ts-ignore */}
+                        <svg ref={barRef}/>
+                    </div>
                 </div>
             ) : (
                 <span>No Statistics!</span>
