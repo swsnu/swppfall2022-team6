d7b4342adb34c6fe4cb9998ceccc1bc77375dad5
lilys012 <70987936+lilys012@users.noreply.github.com>
2022-11-05 17:21:38 +0900

    Feature/ice/postchain (#58)
    * Trying toggle chain
    
    * Post Chain Implemented
    
    * Post Test Almost Done
    
    * remove unnecessary code
    
    Co-authored-by: ice1039 <jl6880@daum.net>
    Co-authored-by: Cheeun Lim <86710785+ice1039@users.noreply.github.com>

--- frontend/src/components/Post/Post.test.tsx
+++ frontend/src/components/Post/Post.test.tsx
@@ -1,72 +1,155 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router";
 import Post from "./Post";
+
+const mockNavigate = jest.fn();
+jest.mock("react-router", () => ({
+    ...jest.requireActual("react-router"),
+    useNavigate: () => mockNavigate,
+}));
 
 describe("<Post />", () => {
     it("should render without errors", () => {
-      render(<Post 
-        user_name={"User Name"}
-        content={"Post Content"}
-        location={"User Loc"}
-        time ={"Date and Time"}
-        id = {1}
-        image={""}
-        reply_to={1}
-        chain_open={false}
-        />);
+      render(
+        <MemoryRouter>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                  <Post 
+                  user_name={"User Name"}
+                  content={"Post Content"}
+                  location={"User Loc"}
+                  created_at ={"Date and Time"}
+                  id = {1}
+                  image={""}
+                  reply_to={null}
+                  />
+                }
+            />
+          </Routes>
+        </MemoryRouter>
+      );
       const username = screen.getByText("User Name"); 
       const postContent = screen.getByText("Post Content"); 
       const postLoc = screen.getByText("User Loc");
       const dtText = screen.getByText("Date and Time");
-      screen.getByText("Show All"); 
       expect(username).toBeInTheDocument();
       expect(postContent).toBeInTheDocument();
       expect(postLoc).toBeInTheDocument();
       expect(dtText).toBeInTheDocument();
     });
     it("should not render toggleChainButton when there are no replies", () => {
-        render(<Post 
-            user_name={"User Name"}
-            content={"Post Content"}
-            location={"User Loc"}
-            time ={"Date and Time"}
-            id = {1}
-            image={""}
-            reply_to={0}
-            chain_open={false}
-            />);
-          const username = screen.getByText("User Name"); 
-          const postContent = screen.getByText("Post Content"); 
-          const postLoc = screen.getByText("User Loc");
-          const dtText = screen.getByText("Date and Time");
+        render(
+        <MemoryRouter>
+          <Routes>
+              <Route
+                  path="/"
+                  element={
+                  <Post 
+                    user_name={"User Name"}
+                    content={"Post Content"}
+                    location={"User Loc"}
+                    created_at ={"Date and Time"}
+                    id = {1}
+                    image={""}
+                    reply_to={null}
+                  />
+                  }
+            />
+          </Routes>
+        </MemoryRouter>
+        );
           const toggleChainButton = screen.queryAllByText("Show All"); 
-          expect(username).toBeInTheDocument();
-          expect(postContent).toBeInTheDocument();
-          expect(postLoc).toBeInTheDocument();
-          expect(dtText).toBeInTheDocument();
           //The toggleChainButton should not be in the document when reply_to is 0
           expect(toggleChainButton).toHaveLength(0);
     });
     it("should render show all when there are replies, and chain is not open", () => {
-        render(<Post 
-            user_name={"User Name"}
-            content={"Post Content"}
-            location={"User Loc"}
-            time ={"Date and Time"}
-            id = {1}
-            image={""}
-            reply_to={1}
-            chain_open={false}
-            />);
+        render(        
+        <MemoryRouter>
+          <Routes>
+            <Route
+                path="/"
+                element={
+                <Post 
+                  user_name={"User Name"}
+                  content={"Post Content"}
+                  location={"User Loc"}
+                  created_at ={"Date and Time"}
+                  id = {1}
+                  image={""}
+                  reply_to={1}
+                />
+                }
+            />
+          </Routes>
+        </MemoryRouter>
+        );
           const username = screen.getByText("User Name"); 
-          const postContent = screen.getByText("Post Content"); 
-          const postLoc = screen.getByText("User Loc");
-          const dtText = screen.getByText("Date and Time");
-          expect(username).toBeInTheDocument();
-          expect(postContent).toBeInTheDocument();
-          expect(postLoc).toBeInTheDocument();
-          expect(dtText).toBeInTheDocument();
           expect(username.classList.contains("reply_to")).not.toBe(0);
           expect(username.classList.contains("chain_open")).toBe(false);
           screen.getByText("Show All");
+    });
+    it("should open the chain properly", () => {
+      render(        
+        <MemoryRouter>
+          <Routes>
+            <Route
+                path="/"
+                element={
+                <Post 
+                  user_name={"User Name"}
+                  content={"Post Content"}
+                  location={"User Loc"}
+                  created_at ={"Date and Time"}
+                  id = {1}
+                  image={""}
+                  reply_to={1}
+                  />
+                }
+            />
+          </Routes>
+        </MemoryRouter>
+      );
+      const toggleChainButton = screen.getByText("Show All");
+      fireEvent.click(toggleChainButton);
+      // should edit after chain backend is implemented
+      const chainUsername = screen.getByText("WeatherFairy"); 
+      const chainPostContent = screen.getByText("Original Post..."); 
+      const chainPostLoc = screen.getByText("Location");
+      const chainDtText = screen.getByText(new Date().toLocaleDateString());
+      expect(chainUsername).toBeInTheDocument();
+      expect(chainPostContent).toBeInTheDocument();
+      expect(chainPostLoc).toBeInTheDocument();
+      expect(chainDtText).toBeInTheDocument();
+      const newToggleChainButton = screen.getByText("Close All");
+      expect(newToggleChainButton).toBeInTheDocument();
+    });
+    it("should navigate to the post on click", () => {
+      render(        
+        <MemoryRouter>
+          <Routes>
+            <Route
+                path="/"
+                element={
+                <Post 
+                  user_name={"User Name"}
+                  content={"Post Content"}
+                  location={"User Loc"}
+                  created_at ={"Date and Time"}
+                  id = {1}
+                  image={""}
+                  reply_to={1}
+                  />
+                }
+            />
+          </Routes>
+        </MemoryRouter>
+      );
+      const toggleChainButton = screen.getByText("Show All");
+      fireEvent.click(toggleChainButton);
+      const post = screen.getByText("WeatherFairy");
+      fireEvent.click(post!);
+      expect(mockNavigate).toHaveBeenCalledTimes(1);
     });
 });
