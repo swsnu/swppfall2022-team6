d9594426bdf6418b9c268203862e31b6e2a69720
Seoyoon <seoyoon5635@gmail.com>
2022-11-07 21:40:31 +0900

    Add create hashtag

--- backend/hashtag/views.py
+++ backend/hashtag/views.py
@@ -7,9 +7,13 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 #from rest_framework.decorators import action
 
+from models import Hashtag
+
 class HashtagViewSet(viewsets.GenericViewSet):
     # POST /hashtag/
     @transaction.atomic
     def create(self, request):
-        del request
-        return Response("create hashtag", status=status.HTTP_201_CREATED)
+        Hashtag.objects.create(
+            content=request.POST['content'],
+        )
+        return Response('create hashtag', status=status.HTTP_201_CREATED)
