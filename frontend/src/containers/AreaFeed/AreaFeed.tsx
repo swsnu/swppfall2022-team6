67d80ffb974fd1eb8e112285759d819126553ef8
Seoyoon <seoyoon5635@gmail.com>
2022-11-08 11:41:34 +0900

    Style AreaFeed

--- frontend/src/containers/AreaFeed/AreaFeed.tsx
+++ frontend/src/containers/AreaFeed/AreaFeed.tsx
@@ -159,7 +159,7 @@ function AreaFeed() {
                     {
                         top3Hashtag.map((item, i)=>{
                             return(
-                                <Col key={i}>
+                                <div key={i}>
                                     <button
                                         id={`hashtag${i}-button`}
                                         className="hashtag"
@@ -167,7 +167,7 @@ function AreaFeed() {
                                     >
                                         {"#" + item}
                                     </button>
-                                </Col>
+                                </div>
                             )
                         })
                     }
@@ -177,16 +177,9 @@ function AreaFeed() {
             <Row id="search-box-container">
                 <Row className="area-label">Posts</Row>
                 <Row id="search-container">
-                    <Col>
-                        {/* <input
-                            id="search-box"
-                            value={searchQuery}
-                            placeholder={"🔍 Search"}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                            onKeyPress={(e) => onSubmitSearchBox(e)}
-                        /> */}
+                    <Col md={6}>
                         <CustomSearchBar
-                            className="mapsearch-searchbar"
+                            className="search-box"
                             value={searchQuery}
                             onChange={(searchVal) => setSearchQuery(searchVal)}
                             onCancelSearch={() => onClickClose()}
@@ -194,7 +187,7 @@ function AreaFeed() {
                             placeholder=""
                         />
                     </Col>
-                    <Col>
+                    <Col md={2} id="only-photos-button-container">
                         <button
                             id="only-photos-button"
                             onClick={onSelectOnlyPhotos}
