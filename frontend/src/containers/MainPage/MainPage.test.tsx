3037de37b48b8df75746ca6978036a1cb8526800
Seoyoon Moon <seoyoon5635@gmail.com>
2022-11-11 13:54:38 +0900

    Add MainPage tests (#86)

--- frontend/src/containers/MainPage/MainPage.test.tsx
+++ frontend/src/containers/MainPage/MainPage.test.tsx
@@ -7,27 +7,57 @@ jest.mock("react-router", () => ({
     ...jest.requireActual("react-router"),
     useNavigate: () => mockNavigate,
 }));
+
+jest.mock("../../components/Map/Map", ()=>(props: any)=><div>MapSearch</div>);
+jest.mock("../../components/MapSearch/MapSearch", ()=>(props: any)=><div>MapSearch</div>);
+jest.mock("../../components/ReportModal/ReportModal", ()=>(props: any)=><div>MapSearch</div>);
 console.log = jest.fn();
 
-// const mockGeolocation = {
-//   getCurrentPosition: jest.fn(),
-// };
-
-// (global.navigator as unknown as jest.Mock).mockReturnValue({
-//   geolocation: mockGeolocation
-// })
+const kakao = {
+    maps: {
+      services: {
+        Geocoder: jest.fn(),
+        Status: {
+          OK: "OK",
+          ZERO_RESULT: "ZERO_RESULT",
+          ERROR: "ERROR",
+        }},
+    },
+  };
 
 describe("<MainPage />", () => {
     beforeEach(() => {
+        const mockCoord2RegionCode = jest.fn((lng, lat, callback)=>callback(
+            [{
+                address_name: "서울특별시 관악구 신림동"
+            }],
+            "OK",
+        ));
         jest.clearAllMocks();
+        global.kakao = kakao as any;
+        (kakao.maps.services.Geocoder as jest.Mock).mockReturnValue({
+            coord2RegionCode: mockCoord2RegionCode,
+        });
     });
-    it("should render withour errors", () => {
+    it("should render without errors", () => {
         const { container } = render(<MainPage />);
         expect(container).toBeTruthy();
     });
+    it("should not set address if kakao API error", () => {
+        const mockCoord2RegionCodeError = jest.fn((lng, lat, callback)=>callback(
+            [{
+                address_name: ""
+            }],
+            "ERROR",
+        ));
+        (kakao.maps.services.Geocoder as jest.Mock).mockReturnValue({
+            coord2RegionCode: mockCoord2RegionCodeError,
+        });
+        render(<MainPage />);
+    });
     it("should handle MyPage button", async () => {
         render(<MainPage />);
-        const myPageBtn = screen.getByRole("button", { name: "MyPage" });
+        const myPageBtn = screen.getByLabelText("mypage-button");
         fireEvent.click(myPageBtn!);
         await waitFor(() =>
             expect(mockNavigate).toHaveBeenCalledWith(`/mypage`)
@@ -35,7 +65,7 @@ describe("<MainPage />", () => {
     });
     it("should handle findout button", async () => {
         render(<MainPage />);
-        const findoutBtn = screen.getByRole("button", { name: "Find out" });
+        const findoutBtn = screen.getByLabelText("findout-button");
         fireEvent.click(findoutBtn!);
         await waitFor(() =>
             expect(mockNavigate).toHaveBeenCalledWith(`/areafeed`)
@@ -44,8 +74,16 @@ describe("<MainPage />", () => {
     it("should handle report button", async () => {
         render(<MainPage />);
         console.error = jest.fn();
-        const reportBtn = screen.getByRole("button", { name: "Report" });
+        const reportBtn = screen.getByRole("button", { name: "Report!" });
         fireEvent.click(reportBtn!);
+    });
+    it("should close search result if background clicked", async () => {
+        render(<MainPage />);
+        console.error = jest.fn();
+        const backGrounds = screen.getAllByLabelText("background");
+        for(let i=0; i<backGrounds.length; i++){
+            fireEvent.click(backGrounds[i]!);
+        }
     });
     it("should change slider properly", async () => {
         render(<MainPage />);
