aa223daee08f80293eee6463d1ddedaf435f2f83
Seoyoon <seoyoon5635@gmail.com>
2022-11-14 02:03:39 +0900

    Add provider to indes.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
