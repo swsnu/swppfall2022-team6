0756b297c87ed33d7cc68f38bf0dcaa8bf2ab0d9
ice1039 <jl6880@daum.net>
2022-11-23 14:22:38 +0900

    navbar elements

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faBullhorn, faUser } from "@fortawesome/free-solid-svg-icons";
 
 function NavigationBar() {
-    return <div></div>;
+    const navigate = useNavigate();
+    const handleClickHome = () => {
+        navigate("/");
+    }
+    const handleClickMyPage = () => {
+        navigate("/mypage");
+    }
+    const handleClickReport = () => {
+        // TODO: open report modal in postdetail & arefeed -> navigate to areafeed
+    }
+    return (
+    <div id="navbar-container">
+        <ButtonGroup>
+            <Button onClick={handleClickHome}>
+                <FontAwesomeIcon icon={faHouse} />
+            </Button>
+            <Button>
+                <FontAwesomeIcon icon={faBullhorn}/>
+            </Button>
+            <Button onClick={handleClickMyPage}>
+                <FontAwesomeIcon icon={faUser}/>
+            </Button>
+        </ButtonGroup>
+    </div>
+    )
 }
 
 export default NavigationBar;
+
